2720cd44e0e679c350cb436a980023255c9f5bbc
Rafael S. de Souza <rafael.2706@gmail.com>
2018-11-12 15:51:05 -0500

    :-(

--- STAN_spline.R
+++ STAN_spline.R
@@ -20,27 +20,62 @@ gdat <- data.frame(y=Y,x=X)
 
 stan_data  <- list(Y = Y,
                    X = X,
-                   B = B,
-                   XX = XX,
-                   BB = BB,
-                   N = N,
-                   M = M,
-                   num_basis = num_basis,
-                   num_basis2 = num_basis2)
+                   num_knots = 10,
+                   knots = seq(min(X),max(X),length.out = 10),
+                   spline_degree = 4, 
+                   N = N
+                  )
 
 
 
-stan_model= "data {
-  int N;
-  int M;
-  int num_basis;
-  int num_basis2;
-  vector[N] Y;
-  vector[N] X;
-  vector[M] XX;
-  matrix[num_basis, N] B;
-  matrix[num_basis2, M] BB;
+stan_model= "
+
+functions {
+  vector build_b_spline(real[] t, real[] ext_knots, int ind, int order);
+  vector build_b_spline(real[] t, real[] ext_knots, int ind, int order) {
+  vector[size(t)] b_spline;
+  vector[size(t)] w1 = rep_vector(0, size(t));
+  vector[size(t)] w2 = rep_vector(0, size(t));
+  if (order==1)
+  for (i in 1:size(t))
+  b_spline[i] = (ext_knots[ind] <= t[i]) && (t[i] < ext_knots[ind+1]); 
+  else {
+  if (ext_knots[ind] != ext_knots[ind+order-1])
+  w1 = (to_vector(t) - rep_vector(ext_knots[ind], size(t))) / 
+  (ext_knots[ind+order-1] - ext_knots[ind]);
+  if (ext_knots[ind+1] != ext_knots[ind+order])
+  w2 = 1 - (to_vector(t) - rep_vector(ext_knots[ind+1], size(t))) / 
+  (ext_knots[ind+order] - ext_knots[ind+1]);
+  b_spline = w1 .* build_b_spline(t, ext_knots, ind, order-1) + 
+  w2 .* build_b_spline(t, ext_knots, ind+1, order-1);
 }
+return b_spline;
+}
+}
+
+data {
+  int N;
+  int num_knots;
+  vector[num_knots] knots;
+  int spline_degree; 
+  real Y[N];
+  real X[N];
+}
+
+
+transformed data {
+  int num_basis = num_knots + spline_degree - 1; // total number of B-splines
+  matrix[num_basis, N] B;  // matrix of B-splines
+  vector[spline_degree + num_knots] ext_knots_temp;
+  vector[2*spline_degree + num_knots] ext_knots; // set of extended knots
+  ext_knots_temp = append_row(rep_vector(knots[1], spline_degree), knots);
+  ext_knots = append_row(ext_knots_temp, rep_vector(knots[num_knots], spline_degree));
+  for (ind in 1:num_basis)
+  B[ind,:] = to_row_vector(build_b_spline(X, to_array_1d(ext_knots), ind, spline_degree + 1));
+  B[num_knots + spline_degree - 1, N] = 1; 
+}
+
+
 
 parameters {
 row_vector[num_basis] a_raw;
@@ -50,12 +85,17 @@ real<lower=0> tau;
 
 }
 
+
+
 transformed parameters {
-row_vector[num_basis] a;
+  row_vector[num_basis] a;
 vector[N] Y_hat;
-a = a_raw*tau; 
-Y_hat = a0*X + to_vector(a*B);
+a[1] = a_raw[1];
+for (i in 2:num_basis)
+a[i] = a[i-1] + a_raw[i]*tau; 
+Y_hat = a0*to_vector(X) + to_vector(a*B);
 }
+
 
 model {
 a_raw ~ normal(0, 1);
@@ -65,10 +105,10 @@ Y ~ normal(Y_hat, sigma);
 }
 
 generated quantities{
-vector[M] mu_pred;
+vector[N] mu_pred;
 // Posterior parameter distribution of the mean
 
-mu_pred = a0*XX + to_vector(a*BB);
+mu_pred = a0*to_vector(X) + to_vector(a*B);
 
 }
 
@@ -83,12 +123,15 @@ fit <- stan(model_code = stan_model,
               cores= 3,
               warmup=750)
 
+
+
+
 Y_mean <- extract(fit, "mu_pred")
 Y_mean_cred <- apply(Y_mean$mu_pred, 2, quantile, c(0.05, 0.95))
 Y_mean_mean <- apply(Y_mean$mu_pred, 2, mean)
 
 
-fitdat <- data.frame(x = XX, y = Y_mean_mean, lwr1 = Y_mean_cred[1,],upr1 = Y_mean_cred[2,])
+fitdat <- data.frame(x = X, y = Y_mean_mean, lwr1 = Y_mean_cred[1,],upr1 = Y_mean_cred[2,])
 
 ggplot(data=gdat,aes(x=x,y=y)) +
   geom_point() +
